c5c1b8d1959701f1cd1575bd74a8390fc8467f44
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-10-11 00:54:15 +0530

    Feat : Registration Time & Peaks Peak Days and Hours

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,13 @@ require 'erb'
 
 puts 'Event Manager initialized!'
 
+def clean_time(val, hours, days)
+  time = Time.strptime(val, '%m/%d/%y %k:%M')
+  hours[time.hour] += 1
+  days[time.strftime('%A')] += 1
+  time.strftime('%d-%b-%y %A at %k o\'clock')
+end
+
 def clean_phone_no(number)
   num = number.scan(/[0-9]/).join
   if num.length == 10
@@ -48,9 +55,12 @@ end
 
 if File.exist? 'event_attendees.csv'
   contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+  hours = Hash.new(0)
+  days = Hash.new(0)
   contents.each do |row|
     id = row[0]
     name = row[:first_name]
+    hour = clean_time(row[:regdate], hours, days)
     begin
       number = clean_phone_no(row[:homephone])
       zipcode = clean_zipcode(row[:zipcode])
@@ -62,6 +72,25 @@ if File.exist? 'event_attendees.csv'
       puts "Error rescued :#{name}"
     end
   end
+
+  def print_time_data(data)
+    data.each do |k, v|
+      puts "On #{k} registrations = #{v}"
+    end
+  end
+
+  def find_peak(data)
+    data.select do |k, v|
+      v >= 2
+    end
+  end
+  good_hour = find_peak(hours)
+  good_days = find_peak(days)
+  puts '======Peak Hours======'
+  print_time_data(good_hour)
+  puts '=======Peak Days====='
+  print_time_data(good_days)
+
 else
   puts 'No such file exists.'
 end
